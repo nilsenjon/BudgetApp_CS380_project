468d950eae8fc1128583f7da2e18bbe0462bcde9
nilsenjon <57730020+nilsenjon@users.noreply.github.com>
2020-05-05 14:55:08 -0700

    Add files via upload

---
+++ js/main_budget_page_proto_v1.js
@@ -0,0 +1,15 @@
+//chart script
+var chartPie = document.getElementById("chart1"); //need to fix chart id's and clean up html
+var myChart = new Chart(chartPie, 
+//Chart attributes
+{
+type: 'pie',
+data: {
+    //to be done
+},
+
+                       
+                       
+                       
+
+});
