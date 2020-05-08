9179f3e8176a7ed94c91233d4bb3b6e5cd403437
nilsenjon <57730020+nilsenjon@users.noreply.github.com>
2020-05-07 22:36:04 -0700

    Add files via upload

---
+++ main_budget_page_proto_v1.js
@@ -0,0 +1,24 @@
+//chart script
+
+//placeholder arrays for testing
+var label_array = ["savings", "food", "ex3", "ex4", "ex5"];
+var data_array = [500, 80, 120, 45, 45];
+
+
+var chartPie = document.getElementById("chart1").getContext('2d');
+var myChart = new Chart(chartPie,
+//Chart attributes
+{
+type: 'pie',
+data: {
+    labels: label_array,
+    datasets: [{
+        data: data_array,
+        backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360", "#4D5360"],
+        hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
+              }]
+      },
+options: {
+responsive: true
+}
+});
