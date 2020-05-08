2def42a37196410f12f07e11ffd3f4cf6bc340b1
nilsenjon <57730020+nilsenjon@users.noreply.github.com>
2020-05-08 14:11:54 -0700

    Add files via upload

--- assets/js/main_budget_page_proto_v1.js
+++ assets/js/main_budget_page_proto_v1.js
@@ -22,3 +22,39 @@ options: {
 responsive: true
 }
 });
+
+
+//--------------------------add item to Summary---------------------------------
+
+//red color: #ff0000
+//green color: #24ff00
+
+var btnAdd = document.getElementById("add_budget_item");
+var ul = document.getElementById("summary_budget");
+
+btnAdd.addEventListener("click", function() {
+  var li = document.createElement("li");
+  li.className = 'list-group-item';
+
+  var desc_input = document.getElementById("description_form").value;
+  var val_input = document.getElementById("val_form").value;
+  var date_input = document.getElementById("date_form").value;
+
+  var span_desc = document.createElement("span");
+  span_desc.textContent = desc_input
+  span_desc.className = "float-left"
+  li.appendChild(span_desc);
+
+  var span_date = document.createElement("span");
+  span_date.textContent = date_input
+  span_date.className = "float-left"
+  span_date.style = "margin-left: 50px;"
+  li.appendChild(span_date);
+
+  var span_val = document.createElement("span");
+  span_val.textContent = "$" + val_input
+  span_val.className = "float-right"
+  li.appendChild(span_val);
+
+  ul.insertBefore(li, ul.firstChild);
+})
