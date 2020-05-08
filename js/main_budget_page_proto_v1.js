f7a67128632c11916efa2afb4a78be1eb7bfcf37
nilsenjon <57730020+nilsenjon@users.noreply.github.com>
2020-05-07 22:36:47 -0700

    Add files via upload

--- js/main_budget_page_proto_v1.js
+++ js/main_budget_page_proto_v1.js
@@ -14,7 +14,7 @@ data: {
     labels: label_array,
     datasets: [{
         data: data_array,
-        backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
+        backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360", "#4D5360"],
         hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
               }]
       },
